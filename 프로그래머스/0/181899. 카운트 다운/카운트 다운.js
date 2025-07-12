1a876a0084434042fd19f538c6781c6e8b3b2513
안단테 <98266983+Andante23@users.noreply.github.com>
2025-07-12 18:59:43 +0900

    [level 0] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,8 @@
+function solution(start_num, end_num) {
+    var answer = [];let a = [];
+    for(let i = start_num ; i>=end_num  ; i--){
+        a.push(i);
+    }
+    answer = a;
+    return answer;
+}
